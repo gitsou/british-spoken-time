3c590d8f87f1949a8ab6855de86786e954c3f524
Jacek Ziemianski <jacek.ziemianski@gmail.com>
2025-08-27 19:51:21 +0200

    Small refactors in tests.

--- src/test/java/org/jacekziemianski/bst/SpokenTimeTest.java
+++ src/test/java/org/jacekziemianski/bst/SpokenTimeTest.java
@@ -27,146 +27,140 @@ class SpokenTimeTest {
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0100() {
+    void testGenerateSpokenTime0100() {
         String[] args = {"1:00"};
         assertEquals("one o'clock", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0205() {
+    void testGenerateSpokenTime0205() {
         String[] args = {"02:05"};
         assertEquals("five past two", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0310() {
+    void testGenerateSpokenTime0310() {
         String[] args = {"3:10"};
         assertEquals("ten past three", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0415() {
+    void testGenerateSpokenTime0415() {
         String[] args = {"4:15"};
         assertEquals("quarter past four", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0520() {
+    void testGenerateSpokenTime0520() {
         String[] args = {"05:20"};
         assertEquals("twenty past five", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0625() {
+    void testGenerateSpokenTime0625() {
         String[] args = {"06:25"};
         assertEquals("twenty-five past six", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0632() {
+    void testGenerateSpokenTime0632() {
         String[] args = {"6:32"};
         assertEquals("six thirty-two", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0730() {
+    void testGenerateSpokenTime0730() {
         String[] args = {"07:30"};
         assertEquals("half past seven", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0735() {
+    void testGenerateSpokenTime0735() {
         String[] args = {"07:35"};
         assertEquals("twenty-five to eight", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0840() {
+    void testGenerateSpokenTime0840() {
         String[] args = {"8:40"};
         assertEquals("twenty to nine", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0945() {
+    void testGenerateSpokenTime0945() {
         String[] args = {"9:45"};
         assertEquals("quarter to ten", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime1050() {
+    void testGenerateSpokenTime1050() {
         String[] args = {"10:50"};
         assertEquals("ten to eleven", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime1155() {
+    void testGenerateSpokenTime1155() {
         String[] args = {"11:55"};
         assertEquals("five to twelve", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0000() {
+    void testGenerateSpokenTime0000() {
         String[] args = {"00:00"};
         assertEquals("midnight", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime1200() {
+    void testGenerateSpokenTime1200() {
         String[] args = {"12:00"};
         assertEquals("noon", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime1245() {
+    void testGenerateSpokenTime1245() {
         String[] args = {"12:45"};
         assertEquals("quarter to one", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0015() {
+    void testGenerateSpokenTime0015() {
         String[] args = {"0:15"};
         assertEquals("quarter past midnight", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime1427() {
+    void testGenerateSpokenTime1427() {
         String[] args = {"14:27"};
         assertEquals("two twenty-seven", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime2015() {
+    void testGenerateSpokenTime2015() {
         String[] args = {"20:15"};
         assertEquals("quarter past eight", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime2355() {
+    void testGenerateSpokenTime2355() {
         String[] args = {"23:55"};
         assertEquals("five to midnight", new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime2500() {
+    void testGenerateSpokenTime2500() {
         String[] args = {"25:00"};
-        assertThrows(IllegalArgumentException.class, () -> {
-            new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args);
-        });
+        assertThrows(IllegalArgumentException.class, () -> new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTimeAabb() {
+    void testGenerateSpokenTimeAabb() {
         String[] args = {"aabbccdd"};
-        assertThrows(IllegalArgumentException.class, () -> {
-            new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args);
-        });
+        assertThrows(IllegalArgumentException.class, () -> new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 
     @org.junit.jupiter.api.Test
-    void testgenerateSpokenTime0000_() {
+    void testGenerateSpokenTime0000_() {
         String[] args = {"0000:"};
-        assertThrows(IllegalArgumentException.class, () -> {
-            new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args);
-        });
+        assertThrows(IllegalArgumentException.class, () -> new SpokenTime(outputRules, timeArgumentsParser).generateSpokenTime(args));
     }
 }
